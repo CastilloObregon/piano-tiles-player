040b22adbd9ffaeb48c4aa97c6aeeecccdfc06f2
CastilloObregon <manuelecastilloo@gmail.com>
2020-10-12 18:20:33 -0600

    cambio de lm393

--- app/servoTest/servoLightTest.ino
+++ app/servoTest/servoLightTest.ino
@@ -7,17 +7,24 @@ Servo servo1;
 
 void setup() { // put your setup code here, to run once:
 
+pinMode(8,INPUT);
 // set the servo pin, pin 9, as an servo output pin 
 servo1.attach(9);
 
 }
 void loop() { // put your main code here, to run repeatedly:
+    int temp=digitalRead(8);
+    // Serial.println("Intensity="); //print on serial monitor using ""
+    // Serial.println(temp);         //display output on serial monitor
+    // delay(300);
+    int lightValue = analogRead(A0);
+    Serial.println("Intensity="); //print on serial monitor using ""
+    Serial.println(lightValue);  
 
-int lightValue = analogRead(A0);
-
-// map the light readings to the angle possible by the servo motor 
-lightValue = map (lightValue, 0, 1023, 0, 180);
+    // map the light readings to the angle possible by the servo motor 
+    lightValue = map (lightValue, 0, 1023, 0, 180);
 
 
-// control the servo motor based on the light value read, adjust linearly by angles 
-servo1.write (lightValue); }
+    // control the servo motor based on the light value read, adjust linearly by angles 
+    servo1.write (lightValue); 
+}
